6aa9169a4e05a6ca56f23dbabe3030044263eb6d
rvierbuc <ranlikerats@gmail.com>
2023-07-23 21:12:53 +0000

    finished javascript ch 5

--- projects/eloquent-javascript/chapter-05/exercises.js
+++ projects/eloquent-javascript/chapter-05/exercises.js
@@ -30,16 +30,39 @@ function loop(value, test, update, body) {
 // every ///////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function every() {
+function every(array, test) {
+  //loops through array
+  for(let i = 0; i < array.length; i++){
+  //if the test function on the current item is false, return false
+  if(!test(array[i])){
+  return false;
+}
+}
+//if all the items return true under the test function, return true
+return true;
+}
 
+function every(array, test) {
+  //uses some to check that every element of the array passes the test function
+  return !array.some((element) => !test(element));
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // dominantDirection ///////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function dominantDirection() {
-
+function dominantDirection(string) {
+  // creates a variable that will store the text direction
+let direction = countBy(string, char => {
+  //get the direction of each character in the string
+  let result = characterScript(char.codePointAt(0));
+  //if the character has a direction, return it, otherwise return none
+  return result ? result.direction : 'none';
+});
+//uses reduce to find the direction that is most common
+let dir = direction.reduce((a, b) => a.count > b.count ? a : b).name;
+//returns the direction
+return dir;
 }
 
 // /////////////////////////////////////////////////////////////////////////////
