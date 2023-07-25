4d98bb2e6ab16477f0a8dd926d27f429d723bb98
rvierbuc <ranlikerats@gmail.com>
2023-07-25 04:07:21 +0000

    added operators to studies

---
+++ studies/studies/operators.js
@@ -0,0 +1,34 @@
+//OPERATORS:
+
+//1.Assignment Operators:
+/**
+ * Assignment operators are used to assign a value to a variable. "=" assigns the value
+ * of a variable.
+ */
+
+let x = 10; //assigns x to 10 using the let declaration
+const pi = 3.14; //assigns pi to 3.14 using the const declaration
+var c = 100; //assigns c to 100 using the var declartation
+x = 11 //assigns x to 11
+//
+
+//2. Arithmetic Operators:
+/**Arithmetic operators are used to perform simple mathmatical operations with numeric values.
+ * Common arithmatic operators include +, -, /, *, and %, addition, subtraction, division,
+ * multiplication, and the modulo, respectively.
+ */ 
+
+let a = 3 * 2;
+console.log(a);
+//this will print 6 to console because 3 multiplied by 2 is 6.
+
+//Comparison Operators:
+
+/**Comparison operators compare two values and return true or false. Common comparison operators
+ * include ===, ==, !==, !=, >, <, >=, and <=, strictly equal, loosely equal, not strictly equal,
+ * not loosely equal, greater than, less than, greater than or equal to, and less than or equal to,
+ * respectively. 
+ */
+
+console.log(5 < 10);
+//this will print true to console because 5 is less than 10.
